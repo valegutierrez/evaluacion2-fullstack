414e895c3d8d95d4b87e844609f395a641a936d3
Valentina <vale.gutierrezc@gmail.com>
2018-10-16 17:59:12 -0300

    grade average done

--- test.rb
+++ test.rb
@@ -1,14 +1,25 @@
 file = File.open('students.csv', 'r')
-print file.readlines.map(&:chomp)
+students = file.readlines
 file.close
-puts 'Ingresa una opción de 1 a 4'
-puts '1: Mostrar datos de alumnos'
-puts '2: Cantidad de asistencias totales'
-puts '3: Estudiantes aprobados'
-puts '4: Salir'
-options = gets.chomp
-# while options != 4
-#     case 1:
-#     case 2:
-#     case 3:
-# end
+options = 0
+while options != 4
+  puts 'Ingresa una opción de 1 a 4'
+  puts '1: Mostrar promedios de alumnos'
+  puts '2: Cantidad de asistencias totales'
+  puts '3: Estudiantes aprobados'
+  puts '4: Salir'
+  options = gets.chomp.to_i
+  case options
+    when 1
+      puts 'Promedios:'
+      students.each do |student|
+        data = student.split(', ').map(&:chomp)
+        grades = data[1..5].inject { |acc, sum| acc.to_i + sum.to_i }
+        average = grades / 5
+        hash = { data[0] => average }
+        puts hash
+      end
+    when 2
+    when 3
+  end
+end
